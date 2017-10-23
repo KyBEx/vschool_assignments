0d6d8b9f085ab418aebcf73975ae8155a339dc69
Owner <owner@Owners-MacBook-Pro.local>
2017-10-23 08:56:34 -0600

    First commit from Friday's exercises

---
+++ exercises/function-practice/app.js
@@ -0,0 +1,88 @@
+// function myName(x) {
+//     console.log(x);
+// };
+//
+// var actualName = myName("Kyle");
+// console.log(actualName);
+
+// function add (num1, num2) {
+//     return num1 + num2;
+// };
+// var sum = add(1, 7);
+// console.log(sum);
+
+// function sub(num1, num2) {
+//     return num1 - num2;
+// };
+// var difference = sub(9, 55);
+// console.log(difference);
+
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// };
+// var product = multiply(9,9);
+// console.log(product);
+
+// function div(num1, num2) {
+//     return num1 / num2;
+// };
+// var dividend = div(9,9);
+// console.log(dividend);
+
+// var sum = (num1, num2) => {
+//     return num1 + num2;
+// }
+// console.log(sum(1,7));
+
+// function checkSize(num1, num2, num3) {
+//     if (num1 > num2 && num1 > num3) {
+//         return num1;
+//     } else if (num2 > num1 && num2 > num3) {
+//         return num2;
+//     } else if (num3 > num1 && num3 > num2) {
+//         return num3;
+//     };
+// };
+//
+// var check = console.log(checkSize(10,9,7));
+
+// var checkEven = (x) => {
+//     if ( x % 2  === 0) {
+//         return "even";
+//     } else {
+//         return "odd";
+//     };
+// };
+// console.log(checkEven(10));
+
+// function stringCheck(check) {
+//     if (check.length <= 20) {
+//         return check + check;
+//     } else {
+//         return check.substr (0, (check.length)/2);
+//     };
+// };
+//
+// console.log(stringCheck("Race race race race "));
+// var raceCheck = "Race race race race r";
+// console.log(raceCheck.length);
+
+// var fibonacci_series = function (n)
+// {
+//   if (n===1)
+//   {
+//     return [0, 1];
+//   }
+//   else
+//   {
+//     var s = fibonacci_series(n - 1);
+//     s.push(s[s.length - 1] + s[s.length - 2]);
+//     return s;
+//   }
+// };
+//
+//  console.log(fibonacci_series(8));
+
+function quadratic (a, b, c) {
+    return a*((x)^2) + (b*x) +c = 0
+}
