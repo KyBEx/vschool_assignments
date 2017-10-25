46f92c640feacd4fc53638092dfcb074f323cfbb
Owner <owner@Owners-MacBook-Pro.local>
2017-10-25 13:20:28 -0600

    Final commit: added keydown eventlistner

--- exercises/dj-js/app.js
+++ exercises/dj-js/app.js
@@ -12,8 +12,33 @@ blueBox.onmouseout = function() {
 
 blueBox.addEventListener("mousedown", function() {
     blueBox.style.backgroundColor = "red";
-})
+});
 
-blueBox.mouseup = function() {
+blueBox.onmouseup = function() {
     blueBox.style.backgroundColor = "yellow";
+};
+
+blueBox.ondblclick = function() {
+    blueBox.style.backgroundColor = "green";
+};
+
+window.addEventListener("wheel", function() {
+    blueBox.style.backgroundColor = "orange";
+});
+
+document.addEventListener("keydown", function(key) {
+    if (key.which === 82) {
+        blueBox.style.backgroundColor = "red";
+    } else if (key.which === 66) {
+        blueBox.style.backgroundColor = "blue";
+    } else if (key.which === 79) {
+        blueBox.style.backgroundColor = "orange";
+    } else if (key.which === 71) {
+        blueBox.style.backgroundColor = "green";
+    } else if (key.which === 89) {
+        blueBox.style.backgroundColor = "yellow";
+    }
 }
+)
+
+console.dir();
