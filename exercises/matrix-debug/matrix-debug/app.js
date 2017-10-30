0441914a6e14acbc25ccdb1cbd104ae99930649e
Kyle Hutchinson <kyle_hutchinson17@yahoo.com>
2017-10-30 10:46:56 -0600

    First commit of the day

---
+++ exercises/matrix-debug/matrix-debug/app.js
@@ -0,0 +1,14 @@
+var matrix = [];
+var rows = 3;
+var columns = 3;
+
+for (var i = 0; i < rows; i++){
+  matrix.push([])
+  // console.log(matrix);
+  for (var j = 0; j < columns; j++){
+    matrix[i].push(0)
+    // console.log(matrix);
+  }
+}
+
+console.log(matrix);
