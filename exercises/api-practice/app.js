9e82f1487bffb14fe54999b712a2618350d45844
Kyle Hutchinson <kyle_hutchinson17@yahoo.com>
2017-11-01 10:34:28 -0600

    Added the axios post, added the page refresh to display the new info in the table

--- exercises/api-practice/app.js
+++ exercises/api-practice/app.js
@@ -11,6 +11,8 @@
 //
 // }
 
+
+
 var title = axios.get("https://api.vschool.io/kyle/todo/").then(function(response) {
     for (var i = 0; i < response.data.length; i++) {
         var table = document.getElementById("chores")
@@ -21,28 +23,26 @@ var title = axios.get("https://api.vschool.io/kyle/todo/").then(function(respons
         cell2.className = 'col2Description'
         var cell3 = row.insertCell(2);
         cell3.className = 'col3Price'
-        var cell4 = row.insertCell(3);
-        cell4.className = 'col4Image';
         var title;
         var description;
         var price;
-        var image;
         title = response.data[i].title;
         document.getElementsByClassName('col1Title')[i].innerText = title;
         description = response.data[i].description;
         document.getElementsByClassName('col2Description')[i].innerText = description;
         price = response.data[i].price;
         document.getElementsByClassName('col3Price')[i].innerText = price;
-        image = response.data[i].imgUrl;
-        document.getElementsByClassName('col4Image')[i].innerText = image;
-
     }
 });
 
-
-
-// var row = table.insertRow(-1);
-// var cell1 = row.insertCell(0);
-// var cell2 = row.insertCell(1);
-// var cell3 = row.insertCell(2);
-// var cell4 = row.insertCell(3);
+var button = document.getElementById("submit");
+button.addEventListener("click", function(){
+    var newTodo = {
+        "title": document.getElementById('newTitle').value,
+        "description": document.getElementById("newDescription").value,
+        "price": document.getElementById("newPrice").value,
+    };
+    axios.post("https://api.vschool.io/kyle/todo/", newTodo).then(function(response){
+        location.reload(true);
+    });
+})
