e62ee31b03c2fe38341de64ef75e79cc44b974f0
Kyle Hutchinson <kyle_hutchinson17@yahoo.com>
2017-11-03 08:59:05 -0600

    commit from yesterday

---
+++ exercises/warm-up/dictionary/app.js
@@ -0,0 +1,12 @@
+var dictionary = {};
+
+var addWord = function(word, definition) {
+    var toCheck = {
+        word: definition
+    };
+    
+}
+
+var search = function(word) {
+    return definition
+}
