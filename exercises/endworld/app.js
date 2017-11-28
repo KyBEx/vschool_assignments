b7a32b791ec1912e03140c90aa1a8bd9aaf4eff8
Kyle Hutchinson <kyle_hutchinson17@yahoo.com>
2017-11-27 18:57:02 -0700

    Finished 24hr countdown clock. Added sound effect and image when timer reaches zero

--- exercises/endworld/app.js
+++ exercises/endworld/app.js
@@ -1,20 +1,40 @@
 
 
 var display = document.getElementById('clock');
-display.innerText = "Countdown appears here";
+display.innerText = "Preparing for coundown";
 let counter = 1;
-let x = 10;
+let x = 20;
+// let date = new Date (null);
+// console.log(date);
+// date.setSeconds(x)
+// let clock = date.toISOString().substr(11,8)
+// console.log(clock)
+
+// date.setSeconds(x);
+// let clock = date.toISOString().substr(11,8)
+
 function countDown() {
-    display.innerText = x;
-    x -= counter
-    console.log(x)
+
+    let date = new Date (null);
+    date.setSeconds(x);
+    let clock = date.toISOString().substr(11,8)
+    display.innerText = clock;
+
+    x -= counter;
+
     if (x === -1) {
-        stop()
+        stop();
+        document.getElementById('endworld').play();
+        document.body.style.backgroundImage = "url('https://www.planetdeadly.com/wp-content/uploads/licorne-nuclear-test.jpg')";
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.backgroundSize = "cover";
+        display.innerText = "THE WORLD HAS ENDED";
     }
 
 }
 
-let timer = setInterval(countDown, 1000);
 function stop () {
     clearInterval(timer)
 }
+
+let timer = setInterval(countDown, 1000);
