032645bfee211ed94d251a749772e8f935d9b85f
Kyle Hutchinson <kyle_hutchinson17@yahoo.com>
2017-12-18 11:29:52 -0700

    last answer

--- exercises/warm-up/findDiff/app.js
+++ exercises/warm-up/findDiff/app.js
@@ -1,14 +1,9 @@
-function findDiff(arr1,...arrs) {
+function findDiff(arr1, ...arrs) {
     const combinedArgs = [].concat.apply([], arrs);
-    const combined = [];
     arr1.push(...combinedArgs)
-    combined.push(...arr1)
-    const unique = combined.filter(item => {
-        if (combined.indexOf(item) === combined.lastIndexOf(item)) {
-            return item
-        }
+    const unique = arr1.filter(item => {
+        return arr1.indexOf(item) === arr1.lastIndexOf(item)
     })
-
     return unique
 }
 
