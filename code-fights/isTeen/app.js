715b7d7a88102fe9e5554cd851910bd981a47d7a
Kyle Hutchinson <kyle_hutchinson17@yahoo.com>
2017-12-24 14:44:29 -0700

    isTeen solution: refactored for readibility and maintainability

---
+++ code-fights/isTeen/app.js
@@ -0,0 +1,15 @@
+// We'll say that a number is 'teen' if it is in the range 13..19 inclusive.
+// Given 3 int values, return true if 1 or more of them are teen.
+
+var hasTeen=function(a, b, c){
+
+    for (item in arguments) {
+        if (arguments[item] >= 13 && arguments[item] <=19) {
+            return true
+        }
+    }
+    return false
+
+}
+
+console.log(hasTeen(1,3,4))
